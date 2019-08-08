20fe100db1e8739a00f252bd57e2955884ae46b3
krishnamurthy98 <krishnaraghu98@gmail.com>
2019-08-08 16:18:34 +0530

    added tweet partial

--- src/main/java/com/dbs/twitter/model/Tweet.java
+++ src/main/java/com/dbs/twitter/model/Tweet.java
@@ -1,6 +1,7 @@
 package com.dbs.twitter.model;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table
@@ -13,4 +14,51 @@ public class Tweet {
     @JoinColumn(name = "t_id" , nullable = false)
     private User user;
 
+    public int getTid() {
+        return tid;
+    }
+
+    public void setTid(int tid) {
+        this.tid = tid;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "Tweet{" +
+                "tid=" + tid +
+                ", message='" + message + '\'' +
+                ", user=" + user +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Tweet tweet = (Tweet) o;
+        return tid == tweet.tid &&
+                Objects.equals(message, tweet.message) &&
+                Objects.equals(user, tweet.user);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(tid, message, user);
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
